44c6346ce634132e6c5a9dd5c203b9188a4e317d
Chihab <chihabedd.ounnit@gmail.com>
2025-05-26 13:00:35 +0200

    main menu

---
+++ src/main/java/com/example/sae201/MainMenuController.java
@@ -0,0 +1,26 @@
+package com.example.sae201;
+
+import javafx.fxml.FXML;
+
+public class MainMenuController {
+    @FXML
+    private void onNouvellePartie() {
+        // TODO: handle new game
+    }
+    @FXML
+    private void onReprendrePartie() {
+        // TODO: handle resume game
+    }
+    @FXML
+    private void onGererDecks() {
+        // TODO: handle manage decks
+    }
+    @FXML
+    private void onParametres() {
+        // TODO: handle settings
+    }
+    @FXML
+    private void onFermer() {
+        // TODO: handle close
+    }
+} 
